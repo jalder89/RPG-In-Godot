4d686ece21a75b7167885bf31bde9a3172a64e10
Jessica Alder <jessalder1989@gmail.com>
2024-06-08 21:03:28 -0700

    Implemented battle animations on the BattleUnit class.

--- Scenes/Battle/BattleUnit.gd
+++ Scenes/Battle/BattleUnit.gd
@@ -1,5 +1,15 @@
 extends Node2D
 class_name BattleUnit
 
+
+@export var battle_animations_scene = PackedScene
+
+var battle_animations : BattleAnimations
+
+func _ready() -> void:
+	if battle_animations_scene is PackedScene:
+		battle_animations = battle_animations_scene.instantiate()
+		add_child(battle_animations)
+
 func melee_attack():
 	print("Attack!")
