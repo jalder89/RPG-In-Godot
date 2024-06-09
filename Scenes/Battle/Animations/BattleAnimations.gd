6cace7e5d98d1b63cbd91d7010c8d1e5cd015d92
Jessica Alder <jessalder1989@gmail.com>
2024-06-08 20:44:09 -0700

    Implemented BattleAnimations class for handling animation playback and signaling.

---
+++ Scenes/Battle/Animations/BattleAnimations.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+class_name BattleAnimations
+
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+
+signal animation_finished
+
+func _ready() -> void:
+	animation_player.animation_finished.connect(
+		func (animation):
+			animation_finished.emit()
+	)
+
+func get_current_animation_length() -> float:
+	return animation_player.current_animation_length / animation_player.speed_scale
+
+func play(animation_name : StringName) -> void:
+	assert(animation_player.has_animation(animation_name))
+	animation_player.play(animation_name)
