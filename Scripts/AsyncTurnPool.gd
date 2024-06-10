b8120529a00ea35ddd28d3c5c889a6ae10f39f8a
Jessica Alder <jessalder1989@gmail.com>
2024-06-09 19:34:15 -0700

    Implemented deal_damage function, BattleUnit death, and battle exit. Updated AsyncTurnPool to only emit turn_over signal if there is an active turn.

--- Scripts/AsyncTurnPool.gd
+++ Scripts/AsyncTurnPool.gd
@@ -9,6 +9,9 @@ func add(node : Node) -> void:
 	active_nodes.append(node)
 
 func remove(node : Node) -> void:
+	if active_nodes.is_empty(): 
+		return
+	
 	active_nodes.erase(node)
-	if active_nodes.is_empty() : emit_signal("turn_over")
+	if active_nodes.is_empty(): emit_signal("turn_over")
 	
--- Scripts/CharacterClasses/ClassStats.gd
+++ Scripts/CharacterClasses/ClassStats.gd
@@ -3,12 +3,30 @@ class_name ClassStats
 
 
 @export var name : String
-@export var max_health : int
+@export var max_health : int:
+	set(value):
+		max_health = value
+		health = max_health
+		emit_signal("max_health_changed")
+		
 @export var attack : int
 @export var defense : int
 @export var speed : int
 
 @export var battle_animations : PackedScene
 
+signal health_changed
+signal no_health
+signal max_health_changed
+
 var level := 1
-var health := 1
+var health := 1:
+	get:
+		return health
+	set(value):
+		health = clamp(value, 0, max_health)
+		emit_signal("health_changed")
+		if health == 0: emit_signal("no_health")
+
+func _init() -> void:
+	self.health = max_health
