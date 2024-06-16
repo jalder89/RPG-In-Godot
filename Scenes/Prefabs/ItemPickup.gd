0414706db07c53f42149909cf583664cf1eaae16
Jessica Alder <jessalder1989@gmail.com>
2024-06-15 20:11:13 -0700

    Implemented barrel items

--- Scenes/Prefabs/ItemPickup.gd
+++ Scenes/Prefabs/ItemPickup.gd
@@ -15,12 +15,5 @@ var inventory : Inventory = ReferenceStash.inventory
 func _run_interaction() -> void:
 	inventory.add_item(item)
 	var article = get_indefinite_article(item.name)
-	Events.emit_signal("request_show_message", "You've found " + article + " " + item.name + "!\n" + item.description)
+	Events.emit_signal("request_show_message", "You've found " + article + " " + item.name + "!")
 	queue_free()
-
-func get_indefinite_article(name) -> String:
-	var first_letter = name.substr(0, 1).to_lower()
-	if first_letter in ["a", "e", "i", "o", "u"]:
-		return "an"
-	else:
-		return "a"
--- Scenes/Prefabs/Items/Barrel.gd
+++ Scenes/Prefabs/Items/Barrel.gd
@@ -1,5 +1,16 @@
 extends Interactable
 
 
+var inventory : Inventory = ReferenceStash.inventory
+
+var has_apple := true
+@export var item : Resource
+
 func _run_interaction() -> void:
-	Events.emit_signal("request_show_message", "It's just a barrel...")
+	if item is Item:
+		inventory.add_item(item)
+		var article = get_indefinite_article(item.name)
+		Events.emit_signal("request_show_message", "You found " + article + " " + item.name + ".")
+		item = null
+	else:
+		Events.emit_signal("request_show_message", "It's just a barrel...")
