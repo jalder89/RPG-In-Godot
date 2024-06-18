7dc7a8384c8bfea1c39a9b50f8fd1c1bbbf0a8a2
Jessica Alder <jessalder1989@gmail.com>
2024-06-17 20:48:51 -0700

    Implemented NPC interactable.

--- Scripts/Interactable.gd
+++ Scripts/Interactable.gd
@@ -4,8 +4,8 @@ class_name Interactable
 func _run_interaction() -> void:
 	pass
 
-func get_indefinite_article(name) -> String:
-	var first_letter = name.substr(0, 1).to_lower()
+func get_indefinite_article(item_name) -> String:
+	var first_letter = item_name.substr(0, 1).to_lower()
 	if first_letter in ["a", "e", "i", "o", "u"]:
 		return "an"
 	else:
